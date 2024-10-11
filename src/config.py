0c8aa749264a1b0917d99b76bb2fb7de38cd5bb6
Gleb Kushedow <kushedow@gmail.com>
2024-10-11 21:31:27 +0300

    Update config.py
    CORS UNSAFE

--- src/config.py
+++ src/config.py
@@ -63,7 +63,7 @@ settings = Settings()
 def setup_middlewares(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.origins,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
